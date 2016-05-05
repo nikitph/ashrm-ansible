6ec9896862d32af89f79ed76c74702e991dd0748
nikit <nikitph@gmail.com>
2016-05-05 16:58:02 -0700

    Update api.sh

--- roles/enferno/templates/api.sh
+++ roles/enferno/templates/api.sh
@@ -9,4 +9,4 @@ GROUP={{user_name}}
 cd /home/{{user_name}}/{{server_hostname}}
 test -d $LOGDIR || mkdir -p $LOGDIR
 source /home/{{user_name}}/{{server_hostname}}/env/bin/activate
-exec gunicorn  --timeout=300 api.py
+exec python api.py
